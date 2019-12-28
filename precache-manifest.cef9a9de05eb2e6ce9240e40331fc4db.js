c96173fcf0ed84bbe6b85f360aaad6f788e45531
ABHIGYAN MAHANTA <abhigyanmahanta001@gmail.com>
2019-12-28 14:41:42 +0530

    Add files via upload

---
+++ precache-manifest.cef9a9de05eb2e6ce9240e40331fc4db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3fe5967ead96b991b14c7778653647e",
+    "url": "/Swag-shop/index.html"
+  },
+  {
+    "revision": "0f69a47fe641771d661e",
+    "url": "/Swag-shop/static/css/main.fe11ebc3.chunk.css"
+  },
+  {
+    "revision": "e6ccd664d5ca74f92ede",
+    "url": "/Swag-shop/static/js/2.af9f5822.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Swag-shop/static/js/2.af9f5822.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f69a47fe641771d661e",
+    "url": "/Swag-shop/static/js/main.8e8ea7f9.chunk.js"
+  },
+  {
+    "revision": "6619fb42c90a52c0d54a",
+    "url": "/Swag-shop/static/js/runtime-main.54255bb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Swag-shop/static/media/logo.5d5d9eef.svg"
+  }
+]);
